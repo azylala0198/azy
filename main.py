1e1e1352cc0d2e6b8c68620cafd4eb7d899b960d
azylala0198 <azyltr31@gmail.com>
2024-02-12 22:50:48 -0800

    sakit s ulo

---
+++ main.py
@@ -0,0 +1,11 @@
+while True: 
+
+	user_input = input("Enter something")
+
+	if user_input == 'hi':
+		print('hello')
+
+	elif user_input == 'goodmorning':
+		print('Good to hear!')
+
+	elif user_input == 'bad':			
---
+++ main2.py
@@ -0,0 +1,30 @@
+import streamlit as st 
+
+st.title('chatbot')
+
+prompt = st.chat_input("Enter something")
+
+if "messages" not in st.session_state:
+	st.session_state.messages = []
+
+for message in st.session_state.messages:
+	user_input = st.chat_message('role')
+	user_input.write(message['content'])	
+
+st.chat_message("user").write(prompt)	
+
+if prompt and prompt != '':
+	st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+
+if prompt == 'hello':
+	st.chat_message("assistant").write('How are you')
+	st.session_state.messages.append({'role': 'assistant', 'content': 'How are you?'})
+if prompt == 'who are you?':
+	st.chat_message("assistant").write('I am your assistant')
+	st.session_state.messages.append({'role': 'assistant', "content": "I am your assistant"})
+elif prompt == 'hi':
+	st.chat_message('assistant').write('Hello!')
+	st.session_state.messages.append({'role:' 'assistant, "content:' "Hello!"})
+
+	
